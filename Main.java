0a1ecfdad96b1de72be5f8e58f436c3dd00f638c
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-05 13:36:55 +0000

    Hailstone pattern

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Hailstone pattern
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +10,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+    //create a scanner
+     Scanner input = new Scanner(System.in);
+    //ask user for integer
+     System.out.println("Please enter a positive integer to see the hailstone sequence");
+    //declare a variablle
+     int n = input.nextInt();
+     //loop statement
+     while (n > 1){
+     if (n % 2 == 0){
+       n = n/2;
+       System.out.println(n);
+      
+      }else if (3*n + 1 > 0){
+        n = n*3 + 1;
+        System.out.println(n);
+
+      }
+      }
+     }
 }
